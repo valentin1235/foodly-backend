dfecf7e5d5a64f15946edded5bddd999978c28ce
Kepler-442b <rosewwross@gmail.com>
2020-02-25 14:10:54 +0900

    initial settings

---
+++ my_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DATABASE 명',
+        'USER': 'DB접속 계정명',
+        'PASSWORD': 'DB접속용 비밀번호',
+        'HOST': '실제 DB 주소',
+        'PORT': '포트번호',
+    }
+}
+
+SECRET_KEY = {
+        'secret':'lgh%1w1g0k#1w!yqx3v2hso^!ta(tb^7^@4&&^rht=2i1vyp%y',
+}
