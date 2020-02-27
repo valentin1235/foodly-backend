35956e7cdb1d3736f44aface8438aa595b926339
smilejakdu <ash982416@gmail.com>
2020-02-27 14:50:57 +0900

    모델 클래스명 다시수정

--- account/models.py
+++ account/models.py
@@ -10,7 +10,7 @@ class User(models.Model):
     create_at = models.DateTimeField(auto_now_add=True, null=True)
     update_at = models.DateTimeField(auto_now=True, null=True)
     is_delete = models.BooleanField(False)
-    user_through_address = models.ManyToManyField('Address', through='User_addresses')
+    user_through_address = models.ManyToManyField('Address', through='User_addresse')
 
     def __str__(self):
         return self.email
@@ -19,7 +19,7 @@ class User(models.Model):
         db_table = 'users'
 
 
-class User_addresses(models.Model):
+class User_addresse(models.Model):
     address = models.ForeignKey('Address', on_delete=models.CASCADE, null=True)
     user = models.ForeignKey('User', on_delete=models.CASCADE, null=True)
 
@@ -39,13 +39,13 @@ class Address(models.Model):
     phone = models.CharField(max_length=45, null=True)
     is_default = models.BooleanField(False, null=True)
     user = models.ForeignKey('User', on_delete=models.CASCADE, null=True)
-    postcode = models.ForeignKey('Postcodes', on_delete=models.CASCADE, null=True)
+    postcode = models.ForeignKey('Postcode', on_delete=models.CASCADE, null=True)
 
     class Meta:
         db_table = 'addresses'
 
 
-class Postcodes(models.Model):
+class Postcode(models.Model):
     postcode = models.CharField(max_length=45)
     shipping_cost = models.DecimalField(max_digits=8, decimal_places=2, null=True)
 
