f4576a860719f399061ae9f22a85dd64db3110a8
smilejakdu <ash982416@gmail.com>
2020-03-02 14:46:02 +0900

    utils 수정

--- account/utils.py
+++ account/utils.py
@@ -1,21 +1,21 @@
 import json
 import jwt
 
-from .models                    import User
+from .models import User
 from foodly_project.my_settings import Algorithm, SECRET_KEY
 
-from django.http                import JsonResponse
+from django.http import JsonResponse
 
 
 def login_check(func):
-
     def wrapper(self, request, *args, **kwargs):
 
         try:
 
             auth_token = request.headers.get('Authorization', None)
             payload = jwt.decode(auth_token, SECRET_KEY, Algorithm)
-            request.user = User.objects.get(id=payload["id"])
+            user = User.objects.get(id=payload["id"])
+            request.user = user
 
             return func(self, request, *args, **kwargs)
 
