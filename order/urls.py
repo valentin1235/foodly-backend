2b4ac123952bae23aa930443f1dd4eb2fff73714
Kepler-442b <rosewwross@gmail.com>
2020-03-02 10:40:42 +0900

    models.py -revised, views.py -CartView added, urls.py - revised

--- order/urls.py
+++ order/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import WishListCreateView
+from .views import WishListCreateView, CartView
 
 urlpatterns = [
     path('/wishlist', WishListCreateView.as_view()),
+    path('/cart', CartView.as_view()),
     ]
