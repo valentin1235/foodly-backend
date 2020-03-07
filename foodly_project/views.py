03cad44fdec48d297859204d7bfc9fe2ff4463c0
valentin1235 <valentin123@naver.com>
2020-03-07 16:58:29 +0900

    homeview code refactoring

--- foodly_project/views.py
+++ foodly_project/views.py
@@ -9,28 +9,30 @@ from products.models import Product, Recipe, Bundle
 
 class HomeView(View):
     def get(self, request):
-        product_data_caching = Product.objects.prefetch_related('category').select_related('season', 'harvest_year', 'measure')
-        bundle_data_caching  = Bundle.objects.prefetch_related('product_set')
-        bundle_info          = bundle_data_caching.values('title', 'price','id')
-        content_info         = [
-                list(data.product_set.values('measure_id__measure', 'name', 'id')
-                    .annotate(Count('name'))) for data in bundle_data_caching
+        product_data_caching = Product.objects.prefetch_related('category').select_related('season', 'harvest_year', 'measure') 
+        bundles = Bundle.objects.all()
+        bundle_deal = [
+                {
+                    'title'   : bundle.title,
+                    'price'   : bundle.price,
+                    'id'      : bundle.id,
+                    'content' : list(bundle.product_set.values('name','id','measure_id__measure'))
+                    } for bundle in bundles
                 ]
-        bundle_deal          = [data for data in zip(bundle_info, content_info)]        
-        category_deal        = product_data_caching.filter(is_main=True, discount_rate='', category__name='frozen').values(
+        category_deal = product_data_caching.filter(is_main=True, discount_rate='', category__name='frozen').values(
                 'id',
                 'name', 
                 'category__name', 
                 'big_image2'
         )
-        seasonal_deal        = product_data_caching.filter(is_main=True, season_id__name='september').values(
+        seasonal_deal = product_data_caching.filter(is_main=True, season_id__name='september').values(
                 'id',
                 'discount_rate', 
                 'name', 
                 'season_id__name', 
                 'big_image1'
         )
-        recommand_product    = product_data_caching.all().order_by('-big_image1').filter(is_main = True).exclude(is_in_stock=0, big_image1='').values(
+        recommand_product = product_data_caching.all().order_by('-big_image1').filter(is_main = True).exclude(is_in_stock=0, big_image1='').values(
                 'id',
                 'price',
                 'name',
